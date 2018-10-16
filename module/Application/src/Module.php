6aee9ba6b88ab74f65f33adf1a5827cbd67e962f
shinkarenko-anton <vsevsevse@ukr.net>
2018-10-16 12:52:18 +0300

    page translation added

--- module/Application/src/Module.php
+++ module/Application/src/Module.php
@@ -7,6 +7,11 @@
 
 namespace Application;
 
+use Zend\ModuleManager\ModuleManager;
+use Locale;
+use Zend\Mvc\MvcEvent;
+use Zend\Http\PhpEnvironment\Request;
+
 class Module
 {
     const VERSION = '3.0.3-dev';
@@ -14,5 +19,39 @@ class Module
     public function getConfig()
     {
         return include __DIR__ . '/../config/module.config.php';
+    }
+
+    // The "init" method is called on application start-up and
+    // allows to register an event listener.
+    public function init(ModuleManager $manager)
+    {
+        // Get event manager.
+        $eventManager = $manager->getEventManager();
+        $sharedEventManager = $eventManager->getSharedManager();
+        // Register the event listener method.
+        $sharedEventManager->attach(__NAMESPACE__, 'dispatch',
+            [$this, 'onDispatch'], 100);
+    }
+
+    // Event listener method.
+    public function onDispatch(MvcEvent $event)
+    {
+        $default   = 'ru';
+        $supported = array('en', 'ru', 'de');
+
+        $request = $event->getRequest();
+
+        //retrieve GET parameter
+        $lang = $request->getQuery('lang', null);
+
+        if (!!($match = Locale::lookup($supported, $lang))) {
+            // The locale is one of our supported list
+            $locale = $match;
+        } else {
+            // Nothing from the supported list is a match
+            $locale = $default;
+        }
+
+        Locale::setDefault(Locale::canonicalize($locale));
     }
 }
